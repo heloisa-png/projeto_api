b75816dc1b03b14136e76ebcc323f6eeed2b2cdb
HELOISA PEREIRA DA SILVA <a95851104@rn.senac.local>
2026-09-16 11:46:01 -0300

    estrutura inicial do projeto

---
+++ config/db.js
@@ -0,0 +1,19 @@
+//configurações externas 
+
+//importando 
+
+*//p maiusculo 
+const {Pool} = require('pg')
+
+require (dotenv).config()
+
+//acesso as variaveis do .env/ configurações do banco 
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT 
+})
+ module.exports = pool 
+
